249ebb474d6470444754e2e734424cc35e6d9b32
stollcode <stevestoll@mail.yahoo.com>
2017-03-10 12:45:47 -0600

    loops and lists practice
    loops and lists

---
+++ lists_and_loops.py
@@ -0,0 +1,35 @@
+python Lists and Loops
+======================
+
+Directions
+----------
+
+Create a document named lists_and_loops.py and save it in your z:\CSP folder.
+
+1) - Use the range() command to create a list named myNumbers, containing numbers
+        between 1 and 50
+   - Loop through the list and print each number.
+
+2) Create a list named myGrades using the following values:
+        88, 91, 57, 36, 93, 83, 74, 32
+
+3) Use a for loop to sum the values in myGrades.
+
+4) Use a for loop to print the highest value from the myGrades list.
+
+5) Use a while loop to print the average value from the myGrades list.
+
+5) Create a list named studentNames using the following names:
+        "Pete", "Joseph", "Stephen", "Wyatt", "Drew", "elijah"
+
+6) Use a while loop to print the name from the studentNames list with the
+        most characters.
+
+7) Use the in operator to determine if the name "Drew" is in the studentNames list.
+        Print "Drew is in the list" if his name is in the studentNames list.
+
+8) Create a 2d list named armorList. The first column must be the armor name,
+    The second column must contain attack speed and the third column must contain
+    damage.
+
+9) Use a nested for loop to print the armorList.
