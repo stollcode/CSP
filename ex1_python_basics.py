8290c27a8caea860ec9877dff28c924c9ab41523
stollcode <stevestoll@mail.yahoo.com>
2017-04-06 11:38:07 -0500

    Basic Python Exercises
    First Try

---
+++ ex1_python_basics.py
@@ -0,0 +1,64 @@
+'''
+python Basics
+======================
+
+Directions
+----------
+Create a module on your z:\ drive named z:\csp\ex1_python_basics.py.  Copy this text and paste it 
+    into the newly created file.
+
+** Problem 1 (Converting kilograms to pounds)**
+
+1) Create a function named kiloToPound(). It should accept a parameter 
+    named kilo.
+2) Use the kilo paramenter to return the number of pounds.
+    One kilogram is equal to 2.204622 pounds.
+3) Call the kiloToPound() function passing in a value for kilo.
+4) Print the value returned from the kiloToPound() function.
+
+Write your code below:
+'''
+
+
+
+
+
+'''
+** Problem 2 (Is Your Car Environmentally Friendly)**
+
+1) Create a function named eFriendly(). It should accept no parameters.
+2) Inside the eFriendly function, use the input() function to:
+  a) Retrieve the number of milesDriven and gallonsUsed.
+  b) Calculate mpg
+  c) If mpg is 32 or greater:
+        Fuel effeciency: value_of_mpg_variable MPG. Awesome!
+     If below 32 mpg print the following message
+        Fuel effeciency: value_of_mpg_variable MPG. Very Sad!
+3) Call the eFriendly() function.
+
+
+Write your code below:
+'''
+
+
+
+'''
+** Problem 3 (Is Space Ship Ready to Launch)**
+
+1) Create a function named launch(). It should accept the following parameters:
+    - launchReady (boolean)
+    - fuelAmount (float)
+2) Use the appropriate logic to determine if it can liftoff. 
+    - Print "Begin Countdown!!!" if it is launchReady and has a fuelAmount > 4000.0
+    - Print detailed error message for each of the following conditions:
+        - It is launchReady but does not have enough fuel.
+        - It has enough fuel but is not launchReady.
+        - It does not have enough fuel and is not launchReady
+3) Call the launch() function passing in the appropriate values.
+4) Call the launch() function four times to test each of possible conditions.
+
+
+Write your code below:
+'''
+
+
