0f5e7a5ee7b13e65a750dbbe3563f6a056ee092f
stollcode <stevestoll@mail.yahoo.com>
2017-04-10 14:01:58 -0500

    Second try

--- while_and_for_loops_exercise.py
+++ while_and_for_loops_exercise.py
@@ -10,7 +10,7 @@ Create a module on your z:\ drive named z:\csp\while_and_for_loops_exercise.py. 
 ** Problem 1 (printNumbers() function)**
 
 1) Create a function named printNumbers() It should accept parameters named 
-    startNumber and endNumber and print two lists between (and uncluding) them.
+    startNumber and endNumber and print two lists between (and including) them.
 2) Using a while loop, print a list of numbers between startNumber and 
     endNumber (inclusive).
 3) Using a for loop, print a list of numbers between startNumber and 
