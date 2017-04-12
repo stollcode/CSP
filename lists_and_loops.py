432c9e2deb2e946b8a993040ea9730a141192160
stollcode <stevestoll@mail.yahoo.com>
2017-04-12 11:34:47 -0500

    Much improved Exercise
    For CSP

--- lists_and_loops.py
+++ lists_and_loops.py
@@ -1,35 +1,139 @@
+"""
 python Lists and Loops
 ======================
 
 Directions
 ----------
+Create a document named lists_and_loops1.py and save it in your z:\CSP folder.
 
-Create a document named lists_and_loops.py and save it in your z:\CSP folder.
 
 1) - Use the range() command to create a list named myNumbers, containing numbers
         between 1 and 50
-   - Loop through the list and print each number.
+   - Loop through the list and print each number on a separate line.
+"""
 
+
+
+
+"""
 2) Create a list named myGrades using the following values:
         88, 91, 57, 36, 93, 83, 74, 32
+"""
 
+
+
+
+"""
 3) Use a for loop to sum the values in myGrades.
+"""
 
+
+
+
+"""
 4) Use a for loop to print the highest value from the myGrades list.
+"""
 
+
+
+
+"""
 5) Use a while loop to print the average value from the myGrades list.
+"""
 
-5) Create a list named studentNames using the following names:
+
+
+
+"""
+6) Create a list named studentNames using the following names:
         "Pete", "Joseph", "Stephen", "Wyatt", "Drew", "elijah"
+"""
 
-6) Use a while loop to print the name from the studentNames list with the
+
+
+
+"""
+7) Use a while loop to print the name from the studentNames list with the
         most characters.
+"""
 
-7) Use the in operator to determine if the name "Drew" is in the studentNames list.
+
+
+
+"""
+8) Use the in operator to determine if studentNames contains the name "Drew".
         Print "Drew is in the list" if his name is in the studentNames list.
+"""
 
-8) Create a 2d list named armorList. The first column must be the armor name,
+
+
+
+"""
+9) Create a string named movies using the following code:
+        movie = "The Saga of the Viking Women and their Voyage to the Waters of the Great Sea Serpent"
+"""
+
+
+
+
+"""
+10) Use the in operator to see if the movie variable contains the word "Viking".  The
+    output should look like:
+
+        movie contains Viking: True
+"""
+
+
+
+
+"""
+11) Use the in operator to see if the movie variable contains the word "men". The output
+    should look like:
+
+        movie contains men: True
+"""
+
+
+
+
+"""
+12) Use the in operator to see if the movie variable contains the word "Men". The output
+    should look like:
+
+        movie contains Men: False
+"""
+
+
+
+
+"""
+13) Create a tuple (not a list) named   grades   containing the following values:
+    88, 55, 26, 98, 90, 87
+"""
+
+
+
+
+"""
+14) Set the fourth value from 98 to 89 and print the   grades   tuple.
+"""
+
+
+
+
+"""
+15) Create a 2d list named armorList. The first column must contain the armor name,
     The second column must contain attack speed and the third column must contain
     damage.
+"""
 
-9) Use a nested for loop to print the armorList.
+
+
+
+"""
+16) Use a nested for loop to print the armorList using column headings.
+
+"""
+
+
+
